f872313b9886a319e55ac59c049662d460015a10
Oscar Ottonello <105860382+osqui91@users.noreply.github.com>
2023-03-28 13:41:29 +0000

    first commit

---
+++ lipsum.sh
@@ -0,0 +1,6 @@
+for i in {1..5}
+do
+	lineas=$(shuf -i 1-20 -n 1)		
+	lorem=$(curl -s https://www.lipsum.com/feed/xml\?amount\=$lineas | sed -n '/<lipsum>/,/<\/lipsum>/p' | sed -e 's/<lipsum>//g' -e 's/<\/lipsum>//g')   
+	echo "$lorem" >> loremipsum-$i.txt
+done
